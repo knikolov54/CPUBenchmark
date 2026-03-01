1136cdc9e59f4fc2552d41a8efbaf2824bc98442
knikolov54 <krisnikolov5407@gmail.com>
2026-03-01 10:16:22 +0200

    changes

---
+++ CPUBenchmark/Models/Tests/CpuIntensiveMath.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace CPUBenchmark.Models;
+
+public class CpuIntensiveMath : ICpuTest
+{
+    public TestResult Run()
+    {
+        long iterations = 500_000_000;
+        double result = 0;
+
+        var timer = new Stopwatch();
+        timer.Start();
+
+        for (long i = 0; i < iterations; i++)
+        {
+            result += Math.Sqrt(i) * Math.Sin(i) / (Math.Cos(i) + 1.000001);
+        }
+
+        timer.Stop();
+
+        return new TestResult
+        {
+            TestType = TestType.CpuIntensiveMath,
+            Value = timer.ElapsedMilliseconds
+        };
+    }
+}
